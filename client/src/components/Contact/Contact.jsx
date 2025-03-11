16dedda18f250231dca3540da66489ce3272fe85
Jishnu <jishnukhargharia11@gmail.com>
2025-03-11 19:19:31 +0530

    responsive contact us page

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="bg-[#272928]">
-      <div className="flex flex-col md:flex-row items-center justify-center w-[90%] mx-auto py-36 gap-20">
-        <div className="w-full md:w-[60%] rounded-lg overflow-hidden shadow-lg relative">
+    <div className="bg-[#272928] py-24 px-5">
+      <div className="flex flex-col sm:flex-col md:flex-row items-center justify-center w-[90%] mx-auto gap-10 sm:gap-16 md:gap-20 lg:gap-24">
+        <div className="w-full sm:w-[80%] md:w-[60%] rounded-lg overflow-hidden shadow-lg relative">
           <div className="absolute inset-0 bg-black opacity-40 pointer-events-none"></div>
 
           <iframe
@@ -15,37 +15,23 @@ const Contact = () => {
             allowFullScreen={true}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            className="w-full h-[550px] rounded-2xl"
+            className="w-full h-[550px] md:h-[450px] lg:h-[600px] rounded-2xl"
           />
 
-          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[85%] md:w-[75%] text-white p-6 rounded-3xl shadow-xl border border-[rgba(237,90,45,0.8)] bg-[#252525] backdrop-blur-3xl">
-            <div className="flex flex-col md:flex-row justify-between items-start text-left gap-32">
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[90%] sm:w-[85%] md:w-[75%] lg:w-[70%] text-white p-6 sm:p-7 lg:p-8 rounded-3xl shadow-xl border border-[rgba(237,90,45,0.8)] bg-[#252525] backdrop-blur-3xl">
+            <div className="flex flex-col md:flex-row justify-between items-start text-left gap-8 sm:gap-10 md:gap-32 lg:gap-40">
               <div className="w-full md:w-1/2">
-                <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">
-                  Address
-                </h2>
-                <p className="text-lg text-white">
-                  E-Yantra Robotics Lab, Siddaganga Institute of Technology,
-                  Tumkur, Karnataka
-                </p>
+                <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">Address</h2>
+                <p className="text-lg text-white">E-Yantra Robotics Lab, Siddaganga Institute of Technology, Tumkur, Karnataka</p>
               </div>
 
               <div className="w-full md:w-1/2 mt-4 md:mt-0 flex flex-col items-start">
                 <div className="mb-2">
-                  <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">
-                    Email
-                  </h2>
-                  <a
-                    href="mailto:corsit@sit.ac.in"
-                    className="text-lg text-white hover:text-[rgba(237,90,45,0.8)]"
-                  >
-                    corsit@sit.ac.in
-                  </a>
+                  <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">Email</h2>
+                  <a href="mailto:corsit@sit.ac.in" className="text-lg text-white hover:text-[rgba(237,90,45,0.8)]">corsit@sit.ac.in</a>
                 </div>
                 <div>
-                  <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">
-                    Phone
-                  </h2>
+                  <h2 className="text-sm font-semibold uppercase mb-1 text-gray-300">Phone</h2>
                   <p className="text-lg text-white">8104882160</p>
                 </div>
               </div>
@@ -53,51 +39,26 @@ const Contact = () => {
           </div>
         </div>
 
-
-
-
-
-
-
-
-        <div className="w-full md:w-[40%] border border-[rgba(237,90,45,0.8)] shadow-xl rounded-3xl px-10 py-16 bg-[#1f1f1f] flex flex-col justify-center">
-          <h1 className="text-4xl lg:text-5xl font-semibold text-white mb-6 flex items-center justify-center">
-            <span>Contact Us</span>
-          </h1>
+        <div className="w-full sm:w-[90%] md:w-[40%] border border-[rgba(237,90,45,0.8)] shadow-xl rounded-3xl px-6 sm:px-8 md:px-10 lg:px-12 py-10 sm:py-12 md:py-16 lg:py-20 bg-[#1f1f1f] flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-semibold text-white mb-6 flex items-center justify-center">Contact Us</h1>
 
           <form className="flex flex-col space-y-5">
             <div>
-              <label className="block text-xl font-medium text-gray-300 pl-2 pb-2">
-                Name
-              </label>
-              <input
-                type="text"
-                className="w-full h-12 px-4 text-lg border border-[rgba(237,90,45,0.33)] bg-[#333] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[rgba(237,90,45,0.8)] transition"
-              />
+              <label className="block text-lg sm:text-xl md:text-xl font-medium text-gray-300 pl-2 pb-2">Name</label>
+              <input type="text" className="w-full h-12 px-4 text-lg border border-[rgba(237,90,45,0.33)] bg-[#333] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[rgba(237,90,45,0.8)] transition" />
             </div>
 
             <div>
-              <label className="block text-xl font-medium text-gray-300 pl-2 pb-2">
-                Email
-              </label>
-              <input
-                type="email"
-                className="w-full h-12 px-4 text-lg border border-[rgba(237,90,45,0.32)] bg-[#333] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[rgba(237,90,45,0.8)] transition"
-              />
+              <label className="block text-lg sm:text-xl md:text-xl font-medium text-gray-300 pl-2 pb-2">Email</label>
+              <input type="email" className="w-full h-12 px-4 text-lg border border-[rgba(237,90,45,0.32)] bg-[#333] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[rgba(237,90,45,0.8)] transition" />
             </div>
 
             <div>
-              <label className="block text-lg font-medium text-gray-300 pl-2 pb-2">
-                Message
-              </label>
+              <label className="block text-lg sm:text-xl md:text-xl font-medium text-gray-300 pl-2 pb-2">Message</label>
               <textarea className="w-full h-32 p-4 text-lg border border-[rgba(237,90,45,0.31)] bg-[#333] text-white rounded-3xl focus:outline-none focus:ring-2 focus:ring-[rgba(237,90,45,0.8)] transition"></textarea>
             </div>
 
-            <input
-              type="submit"
-              value="Submit"
-              className="w-[95%] mx-auto px-6 my-2 py-3 border border-[rgba(237,90,45,0.83)] rounded-3xl text-xl font-semibold text-center transition text-white shadow-md hover:scale-105 active:scale-95 cursor-pointer hover:bg-[rgba(237,90,45,0.82)]"
-            />
+            <input type="submit" value="Submit" className="w-full px-6 py-3 border border-[rgba(237,90,45,0.83)] rounded-3xl text-lg sm:text-xl md:text-xl font-semibold text-center transition text-white shadow-md hover:scale-105 active:scale-95 cursor-pointer hover:bg-[rgba(237,90,45,0.82)]" />
           </form>
         </div>
       </div>

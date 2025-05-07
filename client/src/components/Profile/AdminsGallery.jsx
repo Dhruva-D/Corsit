da5712817cb9322345c20066c7fda398c4b6bb82
Dhruva-D <dhruvad575@gmail.com>
2025-05-07 15:52:18 +0530

    fix

--- client/src/components/Profile/AdminsGallery.jsx
+++ client/src/components/Profile/AdminsGallery.jsx
@@ -551,10 +551,7 @@ const AdminsGallery = () => {
                     { content: 'Fourth Year Students:', styles: { fontStyle: 'bold' } }, 
                     { content: `${countRegistrationsBy('year', '4')}`, styles: {} }
                 ],
-                [
-                    { content: 'Latest Registration:', styles: { fontStyle: 'bold' } }, 
-                    { content: getLatestRegistrationDate(), styles: {} }
-                ]
+                
             ];
             
             // Set stats columns width
@@ -1508,15 +1505,7 @@ const AdminsGallery = () => {
                                 }
                             />
                             
-                            <StatCard 
-                                title="Latest Registration" 
-                                value={getLatestRegistrationDate()} 
-                                icon={
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                }
-                            />
+                           
                         </div>
                     )}
                 </div>

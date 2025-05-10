d7b2bd023619de57a014168a62d2843496378df4
Dhruva-D <dhruvad575@gmail.com>
2025-05-10 13:18:15 +0530

    Fixed Date

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -110,7 +110,7 @@ const Home = () => {
                 date: '11 May 2025',
                 title: 'Robotics Workshop',
                 description: 'Learn the basics of robotics and automation in this hands-on workshop.',
-                location: '📍 TEL 101',
+                location: '📍 MBA seminar hall ',
                 status: 'open'
               },
               {
@@ -146,7 +146,7 @@ const Home = () => {
                       <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
                       </svg>
-                      Registration Closed
+                      Coming Soon
                     </span>
                   </div>
                 )}
